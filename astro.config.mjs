dc42b224b7fcbd99b763aa0862189584fb21a5ed
Rafael González <rafa@acromatico.co>
2022-08-17 23:38:12 -0500

    Added blog list

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,14 @@ import svelte from "@astrojs/svelte";
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://rafa.page",
-  integrations: [tailwind(), svelte(), sitemap()]
+  site: "https://rafa.page",
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false,
+    }
+  }), svelte(), sitemap(), mdx()]
 });
