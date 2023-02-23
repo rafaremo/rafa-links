a73a6220f5533c8b1ecd60659433f35de7bec0b4
Rafael González <rafa@acromatico.co>
2023-02-22 22:35:07 -0600

    Migrated to Astro 2.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 import mdx from "@astrojs/mdx";
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://rafa.page",
+  site: "https://rafa.page/",
   integrations: [tailwind({
     config: {
       applyBaseStyles: false,
